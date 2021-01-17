d1256cd7b952a533b1a805ab341ab493c52c6b78
Metoraf007 <36585140+Metoraf007@users.noreply.github.com>
2021-01-16 23:45:55 -0800

    Add files via upload

--- WebSites/Set-WebConfigEcryption.ps1
+++ WebSites/Set-WebConfigEcryption.ps1
@@ -1,30 +1,30 @@
 ﻿# Notes  : Set-WebConfigEcryption
-# Date   : 22.12.2020
+# Date   : 17.01.2020
 # Author : Rotem Simhi
-# Version: 2.0.0.3
+# Version: 2.0.0.4
 
 ##########################################################################################################
 #                                           HOW TO USE                                                   #
 #                                                                                                        #
 #                                                                                                        #
 #   Encrypt appSettings and connectionStrings Sections for all web sites and web applications            #
-#   \.Set-WebConfigEcryption.ps1 -Sections @("appSettings", "connectionStrings")                         #
+#   \.Set-WebConfigEcryption -Sections @("appSettings", "connectionStrings")                             #
 #                                                                                                        #
 #   Encrypt appSettings and connectionStrings Sections                                                   #
-#   \.Set-WebConfigEcryption.ps1 -Site "test" -Sections @("appSettings", "connectionStrings")            #
+#   \.Set-WebConfigEcryption -Site "test" -Sections @("appSettings", "connectionStrings")                #
 #                                                                                                        #
 #   Encrypt appSettings Section                                                                          #
-#   \.Set-WebConfigEcryption.ps1 -Site "test" -Sections "appSettings"                                    #
+#   \.Set-WebConfigEcryption -Site "test" -Sections "appSettings"                                        #
 #                                                                                                        #
 #                                                                                                        #
 #   Decrypt appSettings and connectionStrings Sections for all web sites and web applications            #
-#   \.Set-WebConfigEcryption.ps1 -Sections @("appSettings", "connectionStrings")                         #
+#   \.Set-WebConfigEcryption -Sections @("appSettings", "connectionStrings")                             #
 #                                                                                                        #
 #   Decrypt appSettings and connectionStrings Sections                                                   #
-#   \.Set-WebConfigEcryption.ps1 -Site "test" -Sections @("appSettings", "connectionStrings") -Decrypt   #
+#   \.Set-WebConfigEcryption -Site "test" -Sections @("appSettings", "connectionStrings") -Decrypt       #
 #                                                                                                        #
 #   Decrypt appSettings Section                                                                          #
-#   \.Set-WebConfigEcryption.ps1 -Site "test" -Sections "appSettings" -Decrypt                           #
+#   \.Set-WebConfigEcryption -Site "test" -Sections "appSettings" -Decrypt                               #
 #                                                                                                        #
 ##########################################################################################################
 
@@ -39,18 +39,18 @@ function Get-Runtimeversion{
     )
 
     if ($applicationPoolVersion -eq 'v2.0'){
-        $Runtime = 'v2.0.50727'
+        $runtime = 'v2.0.50727'
     }elseif ($applicationPoolVersion -eq'v4.0') {
-        $Runtime = 'v4.0.30319'
+        $runtime = 'v4.0.30319'
     }else {
-        $Runtime = 'Unknown'
+        $runtime = 'Unknown'
     }
-    return $Runtime
+    return $runtime
 }
 function Get-IISMap {
     $id = 0
     $SiteCollection = @{}
-    $WebSites = Get-Website | where-object {$_.bindings.collection.protocol -like "http*"}
+    $WebSites = Get-Website | where {$_.bindings.collection.protocol -like "http*"}
 
     # Map each of the websites
     foreach ($webSite in $WebSites){
@@ -58,7 +58,7 @@ function Get-IISMap {
         $WebSitePath = $WebSite.physicalPath
         $applicationPoolName = $WebSite.applicationPool
         $applicationPoolVersion = Get-IISAppPool -Name $applicationPoolName | Select-Object -ExpandProperty ManagedRuntimeVersion
-
+        
         $SiteCollection += @{
             $id = @{
                 Name                   = $WebSiteName
@@ -106,6 +106,8 @@ function Set-Encryption  {
     )
 
     $SiteMap = Get-IISMap
+    $SiteMap.values
+
     if ($Site = "All") {
         
         foreach ($id in $SiteMap.GetEnumerator() ){
@@ -114,24 +116,25 @@ function Set-Encryption  {
             $applicationPoolName = $id.Value['applicationPoolName']
             $applicationPoolVersion = $id.Value['applicationPoolVersion']
             
-            # Select the Runtime version
+            Write-Host "Working on Site: $Name, AppPool: $applicationPoolName" -ForegroundColor Green
+
+            # Select the runtime version
             $Runtime = Get-Runtimeversion -applicationPoolVersion $applicationPoolVersion
     
-            if ($Runtime -eq 'Unknown'){
-                Write-Host "Runtime Unknown" -ForegroundColor Red
-                $LocationPath = 'C:\Windows\Microsoft.NET\Framework64\'
+            if ($runtime -eq 'Unknown'){
+                Write-Host "Unknown runtime in $Name"
+                $LocationPath = 'C:\Windows\Microsoft.NET\Framework64'
             }else{
-                $LocationPath = 'C:\Windows\Microsoft.NET\Framework64\{0}' -f ($Runtime)
+                $LocationPath = 'C:\Windows\Microsoft.NET\Framework64\{0}' -f ($runtime)
             }
     
             Set-Location $LocationPath
         
             foreach ($Section in $Sections) {
                 if ($Decrypt) {
-                    Write-Host "Decrypting Site: $Name, with AppPool: $applicationPoolName" -ForegroundColor Green
                    .\aspnet_regiis.exe -pdf $Section $SiteLocation
                 }else {
-                    Write-Host "Encrypting Site: $Name, with AppPool: $applicationPoolName" -ForegroundColor Green
+                Write-Host "Encrypting $Section in Site: $Name, AppPool: $applicationPoolName" -ForegroundColor Green
                    .\aspnet_regiis.exe -pef $Section $SiteLocation -prov DataProtectionConfigurationProvider
                 }
             }
@@ -146,27 +149,26 @@ function Set-Encryption  {
                 $applicationPoolName = $id.applicationPoolName
                 $applicationPoolVersion = $id.applicationPoolVersion
                 
-                # Select the Runtime version
+                # Select the runtime version
                 $Runtime = Get-Runtimeversion -applicationPoolVersion $applicationPoolVersion
   
-                # Generate the path of the aspnet_regiis according to the Runtime version
-                if ($Runtime -eq 'Unknown'){
-                    Write-Host "Runtime Unknown" -ForegroundColor Red
-                    $LocationPath = 'C:\Windows\Microsoft.NET\Framework64\'
+                # Generate the path of the aspnet_regiis according to the runtime version
+                if ($runtime -eq 'Unknown'){
+                    Write-Host "Unknown runtime in $Name"
+                    $LocationPath = 'C:\Windows\Microsoft.NET\Framework64'
                 }else{
-                    $LocationPath = 'C:\Windows\Microsoft.NET\Framework64\{0}' -f ($Runtime)
+                    $LocationPath = 'C:\Windows\Microsoft.NET\Framework64\{0}' -f ($runtime)
                 }
         
                 Set-Location $LocationPath
             
                 foreach ($Section in $Sections) {
                     if ($Decrypt) {
-                        Write-Host "Decrypting Site: $Name, with AppPool: $applicationPoolName" -ForegroundColor Green
-                        .\aspnet_regiis.exe -pdf $Section $SiteLocation
+                    .\aspnet_regiis.exe -pdf $Section $SiteLocation
                     
                     }else {
-                        Write-Host "Encrypting Site: $Name, with AppPool: $applicationPoolName" -ForegroundColor Green
-                        .\aspnet_regiis.exe -pef $Section $SiteLocation -prov DataProtectionConfigurationProvider
+                    Write-Host "Encrypting $Section in Application: $Name, AppPool: $applicationPoolName" -ForegroundColor Green
+                    .\aspnet_regiis.exe -pef $Section $SiteLocation -prov DataProtectionConfigurationProvider
                     }
                 }
             }
@@ -182,6 +184,4 @@ if ($Decrypt){
     # Encrypt
     Set-Encryption -site $Site -Sections $Sections 
 }
-
-
 
